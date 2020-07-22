7f12ffd0880a774e7a05fc67daa757461d470cfb
Ujjwal Ujjain <ujjwal.ujjain@pb.com>
2020-07-22 16:35:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App';
+    num1: number;
+    num2: number;
+    result: number =0;
+    add() {
+        this.result = this.num1 + this.num2;
+        console.log(this.result);
+        
+    }
+    substract() {
+        this.result = this.num1 - this.num2;
+    }
+    multiply() {
+        this.result = this.num1 * this.num2;
+    }
+    divide() {
+        this.result = this.num1 % this.num2;
+    }
 }
